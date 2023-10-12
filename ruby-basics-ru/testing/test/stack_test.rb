dd95bbb5cf6d2162f7bbfbfe90b8776310438a9c
@hexlet/cli <support@hexlet.io>
2023-10-12 21:12:32 +0300

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -5,7 +5,48 @@ require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
-  
+  def test_add_to_empty
+    stack = Stack.new
+    expected = ['ruby']
+    assert { stack.push!('ruby').to_a == expected }
+  end
+
+  def test_add_to_not_empty
+    stack = Stack.new(['ruby'])
+    expected = %w['ruby js']
+    assert { stack.push!('js').to_a == expected }
+  end
+
+  def test_deletion
+    stack = Stack.new %w[ruby js]
+    expected = ['ruby']
+    stack.pop!
+    assert { stack.to_a == expected }
+  end
+
+  def test_lifo_deletion
+    stack = Stack.new %w[ruby js]
+    expected = Stack.new %w[ruby js]
+    stack.push! 'php'
+    stack.pop!
+    assert_equal stack.to_a, expected.to_a
+  end
+
+  def test_empty
+    stack = Stack.new ['php']
+    stack.clear!
+    assert { stack.to_a == [] }
+  end
+
+  def test_emptynes_with_fullfield
+    stack = Stack.new ['ruby']
+    refute { stack.empty? }
+  end
+
+  def test_emptyness_with_empty
+    stack = Stack.new
+    assert { stack.empty? }
+  end
   # END
 end
 
