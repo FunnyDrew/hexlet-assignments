68ec62a61171d8e991ac44eaf8371ccc96c91eda
@hexlet/cli <support@hexlet.io>
2023-11-14 22:43:23 +0300

    submit ruby-basics-ru/metaprogramming

--- ruby-basics-ru/metaprogramming/lib/model.rb
+++ ruby-basics-ru/metaprogramming/lib/model.rb
@@ -1,43 +1,42 @@
 # frozen_string_literal: true
+
 require 'byebug'
 
 # BEGIN
 module Model
-	def self.included(base)
-		base.extend(ClassMethods)
-	end
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
 
-	module ClassMethods
-		def attribute(name, options = {})
-			define_method name do
-				value = self.instance_variable_get "@#{name}"
-				converted_value = value
-				if options[:type] == :datetime
-					converted_value = DateTime.parse(value)
-				end
-				converted_value
-			end
+  module ClassMethods
+    def attribute(name, options = {})
+      define_method name do
+        value = instance_variable_get "@#{name}"
+        converted_value = value
+        converted_value = DateTime.parse(value) if options[:type] == :datetime
+        converted_value
+      end
 
-			define_method "#{name}=" do |value|				
-				self.instance_variable_set "@#{name}", value
-				
-				@params.merge!("#{name}": value)
-			end
-		end
-	end
+      define_method "#{name}=" do |value|
+        instance_variable_set "@#{name}", value
 
-	def initialize(params_list={})
-		instance_variable_set '@params', params_list
-		params_list.each do |name, value|
-			instance_variable_set "@#{name}", value
-		end
-	end
+        @params.merge!("#{name}": value)
+      end
+    end
+  end
 
-	def attributes
-		@params.each do |key, value|
-			@params[key] = self.send(key)
-		end
-	end
+  def initialize(params_list = {})
+    instance_variable_set '@params', params_list
+    params_list.each do |name, value|
+      instance_variable_set "@#{name}", value
+    end
+  end
+
+  def attributes
+    @params.each do |key, _value|
+      @params[key] = send(key)
+    end
+  end
 end
 
 # END
